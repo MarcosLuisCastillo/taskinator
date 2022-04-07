d14ee9a017503bf9b7cad5d75ddf3a6af3bfe773
Marcos Luis Castillo <mcastillo001@csbsju.edu>
2022-04-07 15:12:15 -0500

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,27 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     console.log(event);
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value
+    console.log(taskNameInput);
 
     var listItemE1 = document.createElement("li");
+    // create list  item
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    listItemE1.textContent = taskNameInput;
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+
+    // give it a class name
+    taskInfoE1.className = "task-info";
+
+    //add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemE1.appendChild(taskInfoE1);
+
+    // add enttire list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 

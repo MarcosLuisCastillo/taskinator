9542e52efc3605efe16bb508f7d53a4f81104ad7
Marcos Luis Castillo <mcastillo001@csbsju.edu>
2022-04-07 03:29:05 -0500

    form submit and css js html updates

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var formE1 = document.querySelector("#task-form");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function(event) {
+    console.log(event);
+    event.preventDefault();
+
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(listItemE1);
+};
+
+formE1.addEventListener("submit", createTaskHandler);
